7151635f40d8cfc5ff82e7d702ef85dda106391b
beba2123 <Antenehbeba@gmail.com>
2023-06-14 17:26:04 -0700

    Node updated commit

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
@@ -24,19 +25,19 @@ const countStudents = (dataPath) => {
     }
     const studentEntries = studentPropNames.map((propName, idx) => [
       propName,
-      studentPropValues[idx]
+      studentPropValues[idx],
     ]);
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
   const totalStudents = Object.values(studentGroups).reduce(
-    (pre, cur) => (pre || []).length + cur.length
+    (pre, cur) => (pre || []).length + cur.length,
   );
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(
-      `Number of students in ${field}: ${group.length}. List: ${studentNames}`
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`,
     );
   }
 };
